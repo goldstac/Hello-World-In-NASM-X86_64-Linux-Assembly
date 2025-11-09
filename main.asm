ec057e111b73d85bc79bd9bfc9f9ca0e838f4925
Li Productions <172457372+goldstac@users.noreply.github.com>
2025-11-09 12:30:04 +0400

    Create main.asm

---
+++ main.asm
@@ -0,0 +1,14 @@
+section .data
+msg db "Hello World", 0xA
+len equ $ - msg
+section .text
+global _start
+_start:
+mov rax,1
+mov rdi,1
+lea rsi,[rel msg]
+mov rdx,len
+syscall
+mov rax,60
+mov rdi,0
+syscall
